9234b18ada4124bd5a7ca076567f6dd7f8075414
Is0c <isaacpazb08@gmail.com>
2025-04-08 10:25:40 -0300

    Add files via upload

---
+++ unacifra.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int cifra;
+int main (){
+	int a;
+	cout<<"inserte un numero";
+	cin>>a;
+	if(a<10)
+	{
+	cout<<"tiene 1 cifras"
+	}
+	else if(a<100)
+	{
+	cout<<"tiene 2 cifras"
+	}
+	else if(a<1000)
+    cout<<"tiene 3 cifras"
+}
+
